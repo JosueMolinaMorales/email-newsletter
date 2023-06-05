37591f0a076d0177cdedab205b65c08fba13a80d
Josue Morales <molinajosue92@hotmail.com>
2023-06-04 22:07:19 -0400

    working on authentication

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,26 +1,30 @@
-use std::fmt::Formatter;
-use actix_web::{HttpResponse, post, ResponseError, web};
-use actix_web::http::StatusCode;
-use sqlx::PgPool;
-use anyhow::Context;
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
+use actix_web::http::header::HeaderMap;
+use actix_web::http::StatusCode;
+use actix_web::{post, web, HttpRequest, HttpResponse, ResponseError};
+use anyhow::Context;
+use base64::engine::{self, general_purpose};
+use base64::Engine;
+use secrecy::Secret;
+use sqlx::PgPool;
+use std::fmt::Formatter;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
     title: String,
-    content: Content
+    content: Content,
 }
 
 #[derive(serde::Deserialize)]
 pub struct Content {
     html: String,
-    text: String
+    text: String,
 }
 
 struct ConfirmedSubscriber {
-    pub email: SubscriberEmail
+    pub email: SubscriberEmail,
 }
 
 #[derive(thiserror::Error)]
@@ -38,9 +42,46 @@ impl std::fmt::Debug for PublishError {
 impl ResponseError for PublishError {
     fn status_code(&self) -> StatusCode {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
+}
+
+struct Credentials {
+    username: String,
+    password: Secret<String>,
+}
+
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // The header value, if present, must be a valid UTF8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string")?;
+    let base64encoded_segment = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_bytes = general_purpose::STANDARD
+        .decode(base64encoded_segment)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials =
+        String::from_utf8(decoded_bytes).context("The decoded credential were not valid UTF8.")?;
+
+    // Split into two segments, using ':' as delimiter
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
 }
 
 // Dummy implementation
@@ -48,8 +89,10 @@ impl ResponseError for PublishError {
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
-    email_client: web::Data<EmailClient>
+    email_client: web::Data<EmailClient>,
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
+    let credentials = basic_authentication(request.headers());
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -59,13 +102,13 @@ pub async fn publish_newsletter(
                         &subscriber.email,
                         &body.title,
                         &body.content.html,
-                        &body.content.text
+                        &body.content.text,
                     )
                     .await
                     .with_context(|| {
                         format!("Failed to send newsletter issue to {}", subscriber.email)
                     })?;
-            },
+            }
             Err(error) => {
                 tracing::warn!(
                     error.cause_chain = ?error,
@@ -79,22 +122,22 @@ pub async fn publish_newsletter(
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
 async fn get_confirmed_subscribers(
-    pool: &PgPool
+    pool: &PgPool,
 ) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
     let confirmed_emails = sqlx::query!(
-            r#"
+        r#"
                 SELECT email
                 FROM subscriptions
                 WHERE status = 'confirmed'
             "#,
-        )
-        .fetch_all(pool)
-        .await?
-        .into_iter()
-        .map(|r| match SubscriberEmail::parse(r.email) {
-            Ok(email) => Ok(ConfirmedSubscriber { email }),
-            Err(error) => Err(anyhow::anyhow!(error))
-        })
-        .collect();
+    )
+    .fetch_all(pool)
+    .await?
+    .into_iter()
+    .map(|r| match SubscriberEmail::parse(r.email) {
+        Ok(email) => Ok(ConfirmedSubscriber { email }),
+        Err(error) => Err(anyhow::anyhow!(error)),
+    })
+    .collect();
     Ok(confirmed_emails)
 }
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -3,6 +3,29 @@ use wiremock::{Mock, matchers::{any, path, method}, ResponseTemplate};
 use crate::helpers::{ConfirmationLinks, spawn_app, TestApp};
 
 #[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    // Arrange
+    let app = spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter Title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>"
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(r#"Basic realm="publish""#, response.headers()["WWW-Authenticate"]);
+}
+
+#[tokio::test]
 async fn newsletters_returns_400_for_invalid_data() {
     // Arrange
     let app = spawn_app().await;
